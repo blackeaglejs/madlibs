f88884f631edf904f55cbb4889437837e163688f
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-01-31 09:59:06 -0500

    Add the Madlibs program

---
+++ madlib1.rb
@@ -0,0 +1,18 @@
+puts "Name a place."
+place1 = gets
+puts "Give me a verb."
+verb1 = gets
+puts "Give me a noun."
+noun1 = gets
+puts "Give me a name."
+name1 = gets
+puts "Give me a noun."
+noun2 = gets
+puts "Give me another noun."
+noun3 = gets
+puts "Give me another noun."
+noun4 = gets
+puts "Give me one last noun."
+noun5 = gets
+
+puts "Today I went to #{place1}. I went to #{place1} because I needed to #{verb1} my #{noun1}. While I did that, I ran into my friend #{name1}. My friend suggested that we go eat #{noun2} while trolling a #{noun3}. Unfortunately, while we were trolling, I was attacked by a vicious #{noun4}. I fought the #{noun4} with my gigantic #{noun5}. We won, and my friend and I victoriously ate my #{noun2}."
